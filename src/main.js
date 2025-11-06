751345e4c8a76728b65d4855285c963117efc16c
Desiree <desireerdias@gmail.com>
2025-11-06 15:22:06 -0300

    configuração header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botoes = document.querySelectorAll('[data-tab-button]');
 
+    ocultaElementosDoHeader();
+    const botoes = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const sectionHero = document.querySelector('.hero');
+    const alturaHero = sectionHero.clientHeight;
+    
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader()
+
+        }
+        
+    })
+
+    //section de atrações, programação das abas
     for(let i = 0; i < botoes.length; i++ ){
         botoes[i].addEventListener('click', function(botao){
             const tabTarget = botao.target.dataset.tabButton;
@@ -12,7 +31,28 @@ document.addEventListener('DOMContentLoaded', function(){
 
         })
     }
+    // seção de perguntas e respostas
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const botoes = document.querySelectorAll('[data-tab-button]');
